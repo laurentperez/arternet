6844533996872c94657d3676f1868fd0066fe76a
Laurent Perez <laurent.perez@backelite.com>
2015-05-18 19:16:44 +0200

    wip

--- work/module.js
+++ work/module.js
@@ -1,18 +1,21 @@
 var dog = function() {
 	this.bark = function(){console.log("woof");}
 };
+dog.prototype.wif = function(){console.log("wif wif");}
 
-var mod = (function(){
+var mod = (function(pub){
+
+	var one = function(){console.log("one");};
+	pub.one = one; // can return pub instead of {definitions}
 
 	return {
-	hello : function hello(){
-		console.log("hello");
+ 	one:one,
+	hello : function hello(to){
+		console.log("hello " + to);
 		}
 
 	}
-}());
+}(mod || {}));
 
-
-
-
-console.log("mod");
+console.log(mod.hello("one"));
+console.log(new dog().bark());

5621c2d03eb63796d94c848d186b304d10644a1b
Laurent Perez <laurent.perez@backelite.com>
2015-05-18 20:29:00 +0200

    wip

--- work/module.js
+++ work/module.js
@@ -1,10 +1,15 @@
-var dog = function() {
+// constructor, proto, inherit
+var Dog = function() {
 	this.bark = function(){console.log("woof");}
 };
-dog.prototype.wif = function(){console.log("wif wif");}
+Dog.prototype.wif = function(){console.log("wif wif");}
+var Subdog = function(){Dog.call(this); this.wuf = function(){console.log("sub wuf");}}
+Subdog.prototype = Object.create(Dog.prototype); 
+Subdog.prototype.constructor = Subdog;
 
+// module
 var mod = (function(pub){
-
+	var priv = function(){}; // private
 	var one = function(){console.log("one");};
 	pub.one = one; // can return pub instead of {definitions}
 
@@ -16,6 +21,14 @@ var mod = (function(pub){
 
 	}
 }(mod || {}));
+// fbuzz
+var arr = [1,5,15,27,35,22];
+for (var i=0; i < arr.length; i++) {
+if(arr[i] % 5 === 0) {
+console.log("fizz for " + arr[i]);
+}
+
+}
 
 console.log(mod.hello("one"));
-console.log(new dog().bark());
+console.log(new Dog().bark());

6ff6a4710753647ab1daf1c95d6c7d0ac969e454
Laurent Perez <laurent.perez@backelite.com>
2015-05-18 21:06:16 +0200

    wip

--- work/Fizz.java
+++ work/Fizz.java
@@ -2,15 +2,24 @@ import java.util.*;
 public class Fizz {
 	private static List<Integer> ints = new ArrayList();
 
-	public static void main(String args[]) {
+	public static class Other {
+		public String hello(){
+		return "hello";
+		}
+	}
+	private static Other o = new Other();
+	static {
 	ints.add(1);
 	ints.add(5);
 	ints.add(15);
 	ints.add(27);
 	
-	for(Integer i : ints) {
+	}
+	public static void main(String args[]) {
+	String h = o.hello();
+		for(Integer i : ints) {
 		if(i % 5 == 0) {
-	System.out.println(i);
+	System.out.println(h + " " + i);
 		}
 	}
 	}

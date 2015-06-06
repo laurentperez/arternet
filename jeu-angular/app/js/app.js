dc705126f2c233ba7fed4c29c155f79a60d2bf8d
Laurent Perez <laurent.perez@backelite.com>
2015-06-06 15:46:22 +0200

    wip

--- jeu-angular/app/js/app.js
+++ jeu-angular/app/js/app.js
@@ -3,65 +3,87 @@
 /* App Module */
 
 var gameApp = angular.module('gameApp', [
-  'ngRoute',
-  'gameControllers',
-  'gameFilters'
+    'ngRoute',
+    'gameControllers',
+    'gameFilters'
 ]);
 
-gameApp.factory('service', function($rootScope) {
-  var actor;
-  var actors;
-  var score = 0;
-  return {
-    setActors: function(actors) {
-      this.actors = actors;
-    },
-    getActors: function() {
-      return this.actors;
-    },
-    setActor: function(actor) {
-      this.actor = actor;
-    },
-    getActor: function(id) {
-      for(var i=0; i<this.actors.length; i++) {
-        var aid = this.actors[i].id.toString();
-        if(aid === id) {
-          return this.actors[i];
-        }
-      }
-    },
-    nextActor: function(){
-      var index = this.actors.indexOf(this.actor);
-      console.log("index:" + index);
-      var nextItem;
-     if(index >= 0 && index < this.actors.length - 1) {
-         nextItem = this.actors[index + 1];
-     }
-      return nextItem;
-    },
-    getScore: function() {
-      return this.score;
-    }
+gameApp.factory('service', function ($rootScope) {
 
-  };
+    var actor;
+    var actors;
+    var moviesPlayed = [];
+
+    return {
+        updateMoviePlayed: function(actor, success) {
+            console.log("(updateMoviePlayed) " + actor.id + "/" + success);
+            var played = {
+                actor: actor,
+                success: success
+            }                   ;
+            moviesPlayed.push(played);
+        },
+        getMoviesPlayed: function() {
+            return moviesPlayed;
+        },
+        setActors: function (actors) {
+            this.actors = actors;
+        },
+        getActors: function () {
+            return this.actors;
+        },
+        setActor: function (actor) {
+            this.actor = actor;
+        },
+        getActor: function (id) {
+            for (var i = 0; i < this.actors.length; i++) {
+                var aid = this.actors[i].id.toString();
+                if (aid === id) {
+                    return this.actors[i];
+                }
+            }
+        },
+        nextActor: function () {
+            var index = this.actors.indexOf(this.actor);
+            var nextItem;
+            if (index >= 0 && index < this.actors.length - 1) {
+                nextItem = this.actors[index + 1];
+            }
+            if(nextItem) {
+                console.log("current actor index is:" + index + ", loading next actor, id is:" + nextItem ? nextItem.id: "null (no next actor)");
+            } else {
+                console.warn("there is no next actor")
+            }
+            return nextItem;
+        }
+
+    };
 });
 
 gameApp.config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/home', {
-        templateUrl: 'partials/home.html',
-        controller: 'HomeCtrl'
-      }).
-      when('/actors/:actorId', {
-        templateUrl: 'partials/actor.html',
-        controller: 'ActorCtrl'
-      }).
+    function ($routeProvider) {
+        $routeProvider.
+            when('/home', {
+                templateUrl: 'partials/home.html',
+                controller: 'HomeCtrl'
+            }).
+            when('/score', {
+                templateUrl: 'partials/score.html',
+                controller: 'ScoreCtrl'
+            }).
+            when('/pub/:currentId/:actorId', {
+                templateUrl: 'partials/pub.html',
+                controller: 'PubCtrl'
+            }).
+            when('/actors/:actorId', {
+                templateUrl: 'partials/actor.html',
+                controller: 'ActorCtrl'
+            }).
             when('/play/:actorId', {
-        templateUrl: 'partials/play.html',
-        controller: 'PlayCtrl'
-      }).
-      otherwise({
-        redirectTo: '/home'
-      });
-  }]);
+                templateUrl: 'partials/play.html',
+                controller: 'PlayCtrl'
+            }).
+            otherwise({
+                redirectTo: '/home'
+            });
+    }]);
--- jeu-angular/app/js/controllers.js
+++ jeu-angular/app/js/controllers.js
@@ -1,100 +1,135 @@
-  'use strict';
+'use strict';
 
-  /* Controllers */
+/* Controllers */
 
-  var gameControllers = angular.module('gameControllers', []);
+var gameControllers = angular.module('gameControllers', []);
 
-  gameControllers.controller('HomeCtrl', ['$scope', 'service', '$http',
-    function($scope, service, $http) {
-      $http.get('actors/actors.json').success(function(data) {
-        $scope.actors = data;
-        service.setActors(data);
-        console.log("actors:" + service.getActors());
-        $scope.actor = service.getActors()[0];
-      });
+gameControllers.controller('HomeCtrl', ['$scope', 'service', '$http',
+    function ($scope, service, $http) {
+        $http.get('actors/actors.json').success(function (data) {
+            $scope.actors = data;
+            service.setActors(data);
+            console.log("actors:" + service.getActors());
+            var actors = service.getActors();
+            var firstActor = actors[0];
+            $scope.actor = firstActor;
+        });
     }]);
 
-  gameControllers.controller('PlayCtrl', ['$scope', 'service', '$document', '$location', '$routeParams', '$http',
-    function($scope, service, $document, $location, $routeParams, $http) {
+gameControllers.controller('ActorCtrl', ['$scope', 'service', '$document', '$location', '$routeParams', '$http',
+    function ($scope, service, $document, $location, $routeParams, $http) {
 
-      $scope.actor = service.getActor($routeParams.actorId);
-      var next = service.nextActor();
-      var nextid;
-      console.log("next actor is:" + next);
-      if(next) {
-        nextid = next.id;
-      } else {
-        console.info("no more actors");
-        // todo play range must lead to home
-      }
+        console.log("loading actor from id:" + $routeParams.actorId);
+        var actor = service.getActor($routeParams.actorId);
+        $scope.actor = actor;
+        service.setActor($scope.actor);
 
-      $document.ready(function(){
-        console.log("ready play");
-        $("#playrange").on("input", function(e) {
-          var val = e.currentTarget.value;
-          if(val === "1") {
-            if(nextid) {
-              clearInterval(counter);
-              console.log("will jump game master to:" + nextid);              
-              $location.path("/actors/" + nextid);              
-            } else {
-              $location.path("/score/");
+        $document.ready(function () {
+            console.log("(dom) ready actor" + service);
+            $("#actorrange").on("input", function (e) {
+                var val = e.currentTarget.value;
+                if (val === "1") {
+                    $location.path("/play/" + $scope.actor.id);
+                    $scope.$apply();
+                }
+            });
+
+            $("#photo").on('click', function () {
+                console.log("click" + $scope.actor.id);
+            });
+
+        })
+    }]);
+
+gameControllers.controller('PubCtrl',
+    ['$scope', 'service', '$http', '$routeParams',
+        function ($scope, service, $http, $routeParams) {
+            var current = $routeParams.currentId;
+            var nextid = $routeParams.actorId;
+            $scope.actor = service.getActor($routeParams.currentId);
+            $scope.nextActor = service.getActor($routeParams.actorId);
+
+            console.log("on pub with:" + current + "/" + nextid);
+        }
+    ]);
+
+gameControllers.controller('ScoreCtrl',
+    ['$scope', 'service', '$http', '$routeParams',
+        function ($scope, service, $http, $routeParams) {
+            $scope.moviesPlayed = service.getMoviesPlayed();
+        }
+    ]);
+
+function goToScore(location, scope) {
+    location.path("/score/");
+    scope.$apply();
+}
+
+function goToPub(location, scope) {
+    console.log("(goToPub) will jump to pub:" + scope.actor.id + "/" + scope.nextid);
+    location.path("/pub/" + scope.actor.id + "/" + scope.nextid);
+    scope.$apply();
+}
+
+gameControllers.controller('PlayCtrl', ['$scope', 'service', '$document', '$location', '$routeParams', '$http',
+    function ($scope, service, $document, $location, $routeParams, $http) {
+
+        var actor = service.getActor($routeParams.actorId);
+        $scope.actor = actor;
+        var nextActor = service.nextActor();
+        var nextid;
+        if (nextActor) {
+            nextid = nextActor.id;
+            $scope.nextid = nextid;
+        } else {
+            console.warn("no next actor available");
+        }
+
+        $document.ready(function () {
+            console.log("(dom) ready play");
+            var counter;
+            var count = 10;
+            $("#playrange").on("input", function (e) {
+                var val = e.currentTarget.value;
+                // guess OK
+                if (val === "1") {
+                    clearInterval(counter);
+                    service.updateMoviePlayed(actor, true);
+                    if (nextid) {
+                        goToPub($location, $scope);
+                        return;
+                    } else {
+                        console.log("no more actor, back to score");
+                        $location.path("/score/");
+                        $scope.$apply();
+                    }
+                }
+            });
+
+
+            var timer = function () {
+                count = count - 1;
+                if (count <= 0) {
+                    clearInterval(counter);
+                    // guess KO
+                    // we have a failure to guess, go back to pub
+                    service.updateMoviePlayed(actor, false);
+                    if (nextid) {
+                        goToPub($location, $scope);
+                        return;
+                    } else {
+                        console.log("no more actor, back to score");
+                        $location.path("/score/");
+                        $scope.$apply();
+                    }
+                }
+                if (document.getElementById("timer")) {
+                    document.getElementById("timer").innerHTML = count + " secs"; // watch for spelling
+                }
             }
-            $scope.$apply();
-          }
+            counter = setInterval(timer, 1000);
         });
-
-        var counter;
-        var count = 30;
-        var timer = function()
-        {
-          count=count-1;
-          if (count <= 0)
-          {
-           clearInterval(counter);
-           // alert("timer");
-           // failure, todo back to game master
-           if(nextid) {
-            console.log("will jump game master to:" + nextid);
-            //$location.path("/actors/" + nextid);
-            $scope.$apply();
-           }
-         }
-      document.getElementById("timer").innerHTML=count + " secs"; // watch for spelling
-    }
-    counter=setInterval(timer, 1000);
-  });
 
     }]);
 
-  gameControllers.controller('ActorCtrl', ['$scope', 'service', '$document', '$location', '$routeParams', '$http',
-    function($scope, service, $document, $location, $routeParams, $http) {
 
-      console.log("actor wanted:" + $routeParams.actorId);
-      $scope.actor = service.getActor($routeParams.actorId);
-      service.setActor($scope.actor);
-
-      $document.ready(function(){
-        console.log("ready actor" + service);
-        $("#actorrange").on("input", function(e) {
-          var val = e.currentTarget.value;
-          if(val === "1") {
-            $location.path("/play/" + $scope.actor.id);
-            $scope.$apply();
-          }
-        });
-
-        $("#photo").on('click', function() {
-          console.log("click" + $scope.actor.id);
-        });
-
-      })
-
-      /*
-      $http.get('actors/' + $routeParams.actorId + '.json').success(function(data) {
-        $scope.actor = data;
-        service.setActor(data);
-      });
-      */
-
-    }]);

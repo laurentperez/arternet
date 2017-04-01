b6fcb925b5a1e5d570b5a038a6ed1ac58c9f5713
laurent <laurent@MacBook-Air-de-laurent-2.local>
2017-04-01 19:33:52 +0200

    wip ejs

--- butterfly/index.js
+++ butterfly/index.js
@@ -2,13 +2,41 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var os = require('os');
+var ifaces = os.networkInterfaces();
+var eth;
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+      eth = iface.address;
+    }
+    ++alias;
+  });
+});
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.set('view engine', 'ejs');
+app.get('/', function(req, res){ 
+ res.render('home',{core: eth});
+});
 app.use(express.static(__dirname + '/'));
+
 var http = require('http').createServer(app)
 var io = io.listen(http);
 http.listen(process.env.PORT || 3000);

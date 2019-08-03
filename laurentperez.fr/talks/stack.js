e7bacdeb3164dbccff8d63980c434d41eef2c668
root <root@vps393845.ovh.net>
2019-08-03 18:41:34 +0200

    wip

---
+++ laurentperez.fr/talks/stack.js
@@ -0,0 +1,13 @@
+document.querySelector(".s1").style.opacity=0;
+document.querySelector(".s2").style.opacity=0;
+document.querySelector(".s3").style.opacity=0;
+document.querySelector(".s4").style.opacity=0;
+document.querySelector(".s5").style.opacity=0;
+document.querySelector(".s6a").style.opacity=0;
+document.querySelector(".s6b").style.opacity=0;
+document.querySelector(".s6c").style.opacity=0;
+document.querySelector(".s6d").style.opacity=0;
+document.querySelector(".s6e").style.opacity=0;
+document.querySelector(".s7").style.opacity=0;
+document.querySelector("#three").style.opacity=0;
+

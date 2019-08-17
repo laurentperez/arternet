8b6183e146e03ca2f52e580ea7f5af13b076ee28
laurent <laurent@MacBook-Air-de-laurent.local>
2019-08-18 00:44:22 +0200

    degree

--- laurentperez.fr/talks/cssp/meetup.js
+++ laurentperez.fr/talks/cssp/meetup.js
@@ -3,7 +3,7 @@ class MeetupPainter {
 
   // on récupère les properties dont on a besoin
   static get inputProperties() { 
-    return ['--ranking','list-style-image','--sky']; 
+    return ['--ranking','list-style-image','--sky','--rotate']; 
   }
 
   // la peinture sur un canvas
@@ -11,6 +11,7 @@ class MeetupPainter {
   // la CSS elle ne gère que le style
   paint(ctx, geom, properties) {
     const ranking = properties.get('--ranking');
+    const rot = properties.get('--rotate');
     const sky = properties.get('--sky'); 
     const image = properties.get('list-style-image');
     const spacing = 40;
@@ -20,18 +21,35 @@ class MeetupPainter {
     if(ranking <= 3){color='red'} // c'est déjà un number, pas de cast
     else if(ranking > 3 & ranking <= 5){color='orange'}
 
-    ctx.save();
+    //ctx.save();
+
+    let rotation_degs = rot
+    console.log('deg:' + rotation_degs)
+    let rotation_rads = this.degs_to_rads(rotation_degs)
+    let angle_sine = Math.sin(rotation_rads)
+    let angle_cosine = Math.cos(rotation_rads)
+
     for(let x = 0.5; x < ranking; x++) {
       ctx.globalAlpha = 1/ranking;
       ctx.drawImage(image, 0, 100, 120, 100 );
-      ctx.restore();
+      ctx.setTransform(angle_cosine, angle_sine, -angle_sine, angle_cosine, 10, 10);
+
+      //ctx.restore();
       debugger;
       this.drawStar(ctx,color,x*(size-10 + spacing),1*(size-10 + spacing),5,30,15);
-      console.log('drawn:' + x*(size + spacing), 1*(size + spacing), size, size);
+      //console.log('drawn:' + x*(size + spacing), 1*(size + spacing), size, size);
+      console.log("geom:" + geom.width + "x" + geom.height);
       console.log("type de sky:" + sky);
     }
     
   }
+
+   degs_to_rads (degs) { return degs / (180/Math.PI); }
+   rads_to_degs (rads) { return rads * (180/Math.PI); }
+//| scaleX, skewX,  posX |
+//| skewY,  scaleY, posY |
+//| 0,      0,      1    |
+
 
   drawStar(ctx,color,cx,cy,spikes,outerRadius,innerRadius) {
     var rot=Math.PI/2*3;

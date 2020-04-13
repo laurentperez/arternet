070d90b8b0c9b53d79a5e37ac5f111857f960414
lperez <laurent.perez@sequoiasoft.com>
2020-04-13 14:32:24 +0200

    j14, arrays tree

---
+++ backfront/jdefault/src/test/java/com/example/backfront/BinaryTree.java
@@ -0,0 +1,76 @@
+package com.example.backfront;
+
+/*
+ * using inorder traversal without recursion.
+ * In InOrder traversal first left node is visited, followed by root
+ * and right node.
+ *
+ * input:
+ *      40
+ *     /  \
+ *    20   50
+ *   / \    \
+ *  10  30   60
+ * /   /  \
+ * 5  67  78
+ *
+ * output: 5 10 20 67 30 78 40 50 60
+ */
+public class BinaryTree {
+
+    static class TreeNode {
+        String data;
+        TreeNode left, right;
+
+        TreeNode(String value) {
+            this.data = value;
+            left = right = null;
+        }
+
+    }
+
+    // root of binary tree
+    TreeNode root;
+
+    /**
+     * traverse the binary tree on InOrder traversal algorithm
+     */
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    private void inOrder(TreeNode node) {
+        if (node == null) {
+            System.out.printf("null");
+            return;
+        }
+        System.out.printf("L:");
+        inOrder(node.left);
+        System.out.printf("D:%s ", node.data);
+        System.out.printf("R:");
+
+        inOrder(node.right);
+    }
+
+    /**
+     * Java method to create binary tree with test data
+     *
+     * @return a sample binary tree for testing
+     */
+    public static BinaryTree create() {
+        BinaryTree tree = new BinaryTree();
+        TreeNode root = new TreeNode("40");
+        tree.root = root;
+        tree.root.left = new TreeNode("20");
+        tree.root.left.left = new TreeNode("10");
+        tree.root.left.left.left = new TreeNode("5");
+
+        tree.root.left.right = new TreeNode("30");
+        tree.root.right = new TreeNode("50");
+        tree.root.right.right = new TreeNode("60");
+        tree.root.left.right.left = new TreeNode("67");
+        tree.root.left.right.right = new TreeNode("78");
+
+        return tree;
+    }
+}
--- backfront/jdefault/src/test/java/com/example/backfront/FooTest.java
+++ backfront/jdefault/src/test/java/com/example/backfront/FooTest.java
@@ -5,8 +5,37 @@ import org.junit.jupiter.api.Test;
 public class FooTest {
 
     @Test
-    public void sorting(){
+    public void array(){
+
+        int[] arr = {-1, 1, 5, 8};
+        // linear : for()
+        // bsearch : Arrays/binarySearch()
+        // arrays.asList().contains()
+        // IntStream.of(arr).anyMatch(x -> x == toCheckValue);
+
+        // max : Arrays.stream().max() ou loop arr[0], arr[i] > max , max = arr[i]
+        // sum+= arr[i]
+    }
+
+    @Test
+    public void bsearch() {
         // binary k in arr
         // 
     }
+
+    @Test
+    public void btree() {
+        // https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/?ref=rp
+        // 1 < 2 & 3 -  2 < 4 & 5
+        // Pre order : root first 12453
+        // In order : first visits the left node, followed by root and finally right node. 42513
+        // Post order : left, right finally root 45231
+        BinaryTree bt = BinaryTree.create();
+        bt.inOrder();
+    }
+
+    @Test
+    public void piarc() {
+    }
+
 }
